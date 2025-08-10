c3112582b6f7e410a31eb4ed38260a42726fbfa7
Ashutosh Behera <ashupoco66@gmail.com>
2025-08-10 18:02:18 +0530

    Added the likePost feature

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -6,7 +6,7 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 import { useDispatch } from 'react-redux';
-import { deletePost } from '../../../actions/posts';
+import { deletePost,likePost } from '../../../actions/posts';
 
 const Post = ({post,setCurrentId}) => {
   const classes = useStyles();
@@ -39,13 +39,13 @@ const Post = ({post,setCurrentId}) => {
 </CardContent>
 
         <CardActions className={classes.cardActions}>
-          <Button size = "small" color = "primary" onClick={()=>{}}>
+          <Button size = "small" color = "primary" onClick={()=>dispatch(likePost(post._id))}>
 
             <ThumbUpAltIcon fontSize='small'/>
             Like
             {post.likeCount}
           </Button>
-          <Button size = "small" color = "primary" onClick={()=>dispatch(deletePost(post._id))}>
+          <Button size = "small" color = "secondary" onClick={()=>dispatch(deletePost(post._id))}>
 
             <DeleteIcon fontSize='small'/>
             Delete
