6ce02696a7992b2c28da4032538b2a3b7dfc87e9
Ashutosh Behera <ashupoco66@gmail.com>
2025-08-10 17:16:43 +0530

    DELETE frontend completed

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,7 +2,8 @@ export default (posts = [], action) => {
   switch (action.type) {
     case 'FETCH_ALL':
       return action.payload;
-
+    case 'DELETE':
+        return posts.filter((post)=>post._id!==action.payload)
     case 'CREATE':
       return [...posts, action.payload]; // old posts and add new
 
