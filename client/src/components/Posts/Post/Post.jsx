0b9f13999c74408b66248e4c9d8d284c9657318a
Ashutosh Behera <ashupoco66@gmail.com>
2025-08-16 19:39:38 +0530

    fixed ui of liking post and its logic

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, Box } from '@mui/material';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import ThumbUpAltOutlined from '@mui/icons-material/ThumbUpAltOutlined'
 import DeleteIcon from '@mui/icons-material/Delete';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import moment from 'moment';
@@ -11,7 +12,18 @@ import { usePostStyles } from './styles';
 const Post = ({ post, setCurrentId }) => {
   const dispatch = useDispatch();
   const { card, media, overlay, overlay2, details, title, cardActions } = usePostStyles();
-
+  const user = JSON.parse(localStorage.getItem('profile'))
+  const Likes = () => {
+    if (post?.likes?.length > 0) {
+      return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+        ? (
+          <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+        ) : (
+          <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+        );
+    }
+    return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+  }
   return (
     <Card sx={card}>
       <CardMedia sx={media} image={post.selectedFile} title={post.title} />
@@ -35,9 +47,7 @@ const Post = ({ post, setCurrentId }) => {
       </CardContent>
       <CardActions sx={cardActions}>
         <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
-          <ThumbUpAltIcon fontSize='small' />
-          &nbsp;Like&nbsp;
-          {post.likeCount}
+          <Likes/>
         </Button>
         <Button size="small" color="secondary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize='small' />
