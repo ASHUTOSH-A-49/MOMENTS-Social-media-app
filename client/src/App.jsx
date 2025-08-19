bedc195cfb2ca1c45a4ecb89a46889a43036ab69
Ashutosh Behera <ashupoco66@gmail.com>
2025-08-20 00:55:13 +0530

    auth bug fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { Container } from '@mui/material'; // Updated import from @mui/material
 import Navbar from './components/Navbar/Navbar.jsx';
 import Home from './components/Home/Home.jsx';
-import { BrowserRouter, Route, Routes,redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Routes,Navigate } from 'react-router-dom';
 import Auth from './components/Auth/Auth.jsx';
+import PostDetails from './components/PostDetails/PostDetails.jsx';
 
 const App = () => {
-
+  const user = JSON.parse(localStorage.getItem('profile'))
   return (
     <BrowserRouter>
       <Container maxWidth="lg">
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/auth" exact element={<Auth />} />
+          <Route path="/" exact Component={()=><Navigate to="/posts"/>} />
+          <Route path = "/posts" exact Component={Home}/>
+          <Route path = "/posts/search" exact Component={Home}/>
+          <Route path = "/posts/:id/" Component={PostDetails}/>
+          <Route path="/auth" exact Component={()=>(!user?<Auth/>:<Navigate to="/posts" />)} />
         </Routes>
       </Container>
     </BrowserRouter>
