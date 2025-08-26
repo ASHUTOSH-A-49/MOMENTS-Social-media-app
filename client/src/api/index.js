9be59c8e899ac1ddff069b5045f8a260f0b60af4
Ashutosh Behera <ashupoco66@gmail.com>
2025-08-26 06:22:15 +0530

    api changes during deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,35 +1,23 @@
 import axios from 'axios';
 
+// Update this line to include '/api' at the end of the base URL
+const API = axios.create({ baseURL: `${import.meta.env.VITE_SERVER_URL}/api` });
 
-const API = axios.create({baseURL:import.meta.env.VITE_SERVER_URL})
-
-API.interceptors.request.use((req)=>{
+API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')){
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
     }
 
-
     return req;
-    
-})
+});
 
-// const url = 'http://localhost:5000/posts'
-export const fetchPost = (id)=>API.get(`/posts/${id}`);
-
-export const commentPost = (value,id)=>API.post(`/posts/${id}/commentPost`,{value})
-
-export const fetchPosts = (page)=>API.get(`/posts?page=${page}`);
-
-export const createPost = (newPost)=>API.post('/posts',newPost);
-
-export const updatePost = (id,updatedPost) =>API.patch(`/posts/${id}`,updatedPost)
-
-export const deletePost = (id)=>API.delete(`/posts/${id}`)
-
-export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`)
-
-export const likePost = (id) => API.patch(`/posts/${id}/likePost`)
-
-export const signIn = (formData) => API.post('users/signin',formData)
-
-export const signUp = (formData) => API.post('users/signup',formData)
+export const fetchPost = (id) => API.get(`/posts/${id}`);
+export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
+export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`);
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+export const commentPost = (value, id) => API.post(`/posts/${id}/commentPost`, { value });
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const signIn = (formData) => API.post('/users/signin', formData);
+export const signUp = (formData) => API.post('/users/signup', formData);
