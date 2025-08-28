29a5127d02da3f951bec7ed490699bf402d4a298
Ashutosh Behera <ashupoco66@gmail.com>
2025-08-28 19:02:08 +0530

    changed port variable in index js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
 
 const CONNECTION_URL = process.env.MONGODB_URI;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Connect to the database and start the server
 const startServer = async () => {
